d77fe0836fcf4c09c6c2c22c4e7a0e859617d320
mguimaraesrj <133999934+mguimaraesrj@users.noreply.github.com>
2023-12-01 20:45:07 -0300

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -158,25 +158,32 @@ st.sidebar.title("Start Investor")  # Adiciona título à barra lateral
 ticker_interesse = st.sidebar.text_input("Insira o ticker de interesse (ex: MGLU3):").upper()
 periodo_interesse = st.sidebar.text_input("Insira o período desejado para o histórico de preços (ex: 3mo):")
 
-# Criar instância do AnalisadorDadosMercado
-analisador = AnalisadorDadosMercado()
+if st.sidebar.button("Analisar"):
+    # Criar instância do AnalisadorDadosMercado
+    analisador = AnalisadorDadosMercado()
 
-# Obter dados
-precos, noticias = analisador.baixar_dados(ticker_interesse, periodo_interesse)
+    # Obter dados
+    precos, noticias = analisador.baixar_dados(ticker_interesse, periodo_interesse)
 
-# Simular preços futuros e calcular probabilidade de retorno
-caminhos_precos = analisador.simular_precos(precos)
-prob_retorno = analisador.calcular_retorno_probabilidade(caminhos_precos)
+    # Simular preços futuros e calcular probabilidade de retorno
+    caminhos_precos = analisador.simular_precos(precos)
+    prob_retorno = analisador.calcular_retorno_probabilidade(caminhos_precos)
 
-# Exibindo resultados e plotando gráficos
-st.title("Start Investor")
-
-# Layout com duas colunas
-col1, col2 = st.beta_columns([2, 1])
-
-# Coluna 1: Inputs e resultados
-with col1:
+    # Exibindo resultados e plotando gráficos
     st.write(f"\nProbabilidade de Retorno ser maior ou igual a {analisador.retorno_esperado*100}%: {prob_retorno*100:.2f}%")
+
+    st.write(f"\nÚltimas Notícias para {ticker_interesse} (Limitadas às últimas 10):")
+    noticias = analisador.obter_noticias(ticker_interesse)
+    if noticias:
+        # Criar lista para exibir notícias
+        for i, noticia in enumerate(noticias[:10]):
+            st.write(f"\nNotícia {i + 1}")
+            st.write(f"Título: {noticia['title']}")
+            
+            # Tornar o link clicável usando st.markdown
+            st.markdown(f"Link: [{noticia['link']}]({noticia['link']})")
+            
+            st.write(f"Data: {noticia['date']}")
 
     # Plotar gráfico de histórico de preços
     st.write(f"\nHistórico de Preços para {ticker_interesse} (últimos {periodo_interesse}):")
@@ -190,18 +197,3 @@ with col1:
         title=f'Histórico de Preços para {ticker_interesse}'
     )
     st.altair_chart(chart_precos)
-
-# Coluna 2: Seção de Notícias
-with col2:
-    st.write(f"\nÚltimas Notícias para {ticker_interesse} (Limitadas às últimas 10):")
-    noticias = analisador.obter_noticias(ticker_interesse)
-    if noticias:
-        # Criar lista para exibir notícias
-        for i, noticia in enumerate(noticias[:10]):
-            st.write(f"\nNotícia {i + 1}")
-            st.write(f"Título: {noticia['title']}")
-            
-            # Tornar o link clicável usando st.markdown
-            st.markdown(f"Link: [{noticia['link']}]({noticia['link']})")
-            
-            st.write(f"Data: {noticia['date']}")
